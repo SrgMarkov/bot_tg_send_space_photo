c4fa9872137071483673f1d63c5b7ca4e536a982
SrgMarkov <107784915+SrgMarkov@users.noreply.github.com>
2023-05-31 17:36:32 +0400

    publish photo to Telegram channel

--- telegram-bot.py
+++ telegram-bot.py
@@ -1,9 +1,13 @@
 import os
-from telegram import Bot
+import random
+from telegram import Bot, InputMediaPhoto
 from dotenv import load_dotenv
 
 load_dotenv()
 telegram_token = os.getenv("TELEGRAM_TOKEN")
 bot = Bot(token=telegram_token)
-print(bot.get_me())
-bot.send_message(chat_id='@spacephotocollection', text="Bambaleilo!!!")
+
+dir = 'images'
+random_photo = os.path.join(dir, random.choice(os.listdir(dir)))
+upload_photo = InputMediaPhoto(media=open(f'{random_photo}', 'rb'))
+bot.send_media_group(chat_id='@spacephotocollection', media=[upload_photo])
